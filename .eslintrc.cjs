73cbbc49f6315eff27e90233fc8181c2e93ebafa
Samuel Quiñones <sdquinones1@gmail.com>
2023-02-25 16:00:42 -0500

    improved linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  ignorePatterns: ["node_modules/*", "dist/*", ".astro/*", "*.cjs", "!.prettierrc"],
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:astro/recommended",
     "plugin:astro/jsx-a11y-recommended",
     "plugin:prettier/recommended",
   ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["node_modules/*", "dist/*", "*.cjs", "!.prettierrc"],
+  rules: {
+    "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+    ],
+    "@typescript-eslint/triple-slash-reference": "off",
+  },
   overrides: [
     {
       // Define the configuration for `.astro` file.
@@ -22,24 +36,15 @@ module.exports = {
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+    },
+    {
+      // Define the configuration for `<script>` tag.
+      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+      files: ["**/*.astro/*.js", "*.astro/*.js"],
+      parser: "@typescript-eslint/parser",
       rules: {
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
+        "prettier/prettier": "off",
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
-    ],
-    "@typescript-eslint/triple-slash-reference": "off",
-  },
 };
