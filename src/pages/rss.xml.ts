694c0463520f6e14d03e64ed11d0cb4fd074cdba
Samuel Quiñones <sdquinones1@gmail.com>
2023-02-25 01:39:24 -0500

    told rss to use the content prop

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,5 +1,8 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function get() {
   const posts = await getCollection("blog");
@@ -12,6 +15,7 @@ export async function get() {
       pubDate: post.data.publishDate,
       description: post.data.description,
       link: `/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
     })),
     customData: `<language>en-us</language>`,
   });
