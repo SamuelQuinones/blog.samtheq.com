9653510dfa37b2d05cf3679d20f8f5d600c65251
Samuel Quiñones <sdquinones1@gmail.com>
2023-02-25 16:02:32 -0500

    minor dx improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+const { PUBLIC_SITE_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+
 // https://astro.build/config
 export default defineConfig({
+  site: PUBLIC_SITE_URL,
   markdown: {
     gfm: true,
     remarkPlugins: [],
