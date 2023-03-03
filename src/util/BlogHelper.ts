a4e40d27234da4aff6fc1c4ccdc1a59b7175bf71
Samuel Quiñones <sdquinones1@gmail.com>
2023-03-03 16:49:58 -0500

    standardized code

--- src/util/BlogHelper.ts
+++ src/util/BlogHelper.ts
@@ -4,9 +4,16 @@ export function sortPosts(a: CollectionEntry<"blog">, b: CollectionEntry<"blog">
   return b.data.publishDate.getTime() - a.data.publishDate.getTime();
 }
 
-export function sortTagFilterdPosts(
-  a: CollectionEntry<"blog">["data"],
-  b: CollectionEntry<"blog">["data"]
-) {
-  return b.publishDate.getTime() - a.publishDate.getTime();
+/**
+ * Filter function used to show / hide blog posts from the full post list
+ *
+ * current checks:
+ * - Show ALL if Dev
+ * - Hide if Draft
+ *
+ * @param entry Item in Blog collection
+ * @returns boolean representing if the post should show or not
+ */
+export function getShowablePosts(entry: CollectionEntry<"blog">) {
+  return entry.data.draft !== true || import.meta.env.DEV;
 }
