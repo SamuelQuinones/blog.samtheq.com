b845bdd3a7128803782c5458622063cde5019b27
Samuel Quiñones <sdquinones1@gmail.com>
2023-03-12 01:22:43 -0500

    prepped for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 // Markdown plugins
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import linkify from "rehype-autolink-headings";
 import addA11y from "./plugins/a11y-navigation";
-
+// Astro plugins
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -11,7 +12,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.samtheq.com",
-
   markdown: {
     gfm: true,
     rehypePlugins: [rehypeHeadingIds, [linkify, { behavior: "wrap" }], addA11y],
@@ -26,4 +26,6 @@ export default defineConfig({
     react(),
     sitemap(),
   ],
+  output: "static", // wants to be serverless by default
+  adapter: vercel(),
 });
