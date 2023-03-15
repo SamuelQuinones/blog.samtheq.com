6260a8dfbe7fe5c718b23dca5894680a23fbe7c1
Samuel Quiñones <sdquinones1@gmail.com>
2023-03-14 23:25:20 -0400

    sorted posts in rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,13 +3,14 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-import { getShowablePosts } from "@util/BlogHelper";
+import { getShowablePosts, sortPosts } from "@util/BlogHelper";
 const parser = new MarkdownIt();
 
 export const prerender = true;
 
 export async function get(context: APIContext) {
-  const posts = await getCollection("blog", getShowablePosts);
+  const unsortedPosts = await getCollection("blog", getShowablePosts);
+  const posts = unsortedPosts.sort(sortPosts);
   return rss({
     title: "SamTheQ | Blog",
     description: "My blog site",
