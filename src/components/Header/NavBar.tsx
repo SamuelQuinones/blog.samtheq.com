24332407653701179259eb2a8575f250b4e9b56d
Samuel Quiñones <sdquinones1@gmail.com>
2023-04-12 12:24:14 -0400

    rearranged things

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -3,12 +3,41 @@
 import useOutsideClick from "@hooks/use-outside-click";
 import { type ReactNode, useRef, useState, useCallback } from "react";
 import { motion } from "framer-motion";
-import { navbarVariants, burgerLineClasses, burgerMiddle, burgerBottom, burgerTop } from "./helper";
+import clsx from "clsx";
 
 interface HeaderProps {
   children?: ReactNode;
   title: string;
 }
+
+/** h-8 = 2rem, py-3 is .75 rem on both sides, should total 3.5rem or 56px */
+export const NAVBAR_BASE_HEIGHT = 2 + 2 * 0.75;
+const navbarVariants = {
+  open: { height: "auto" },
+  closed: { height: `${NAVBAR_BASE_HEIGHT}rem` },
+};
+
+const burgerLineClasses = clsx("h-[0.1875rem]", "rounded-full", "w-full", "relative", "bg-white");
+
+const makeTopBottom = (mult: 1 | -1, moveY: number) => {
+  return {
+    open: {
+      y: `${mult * moveY}rem`,
+      rotate: `${mult * 45}deg`,
+    },
+    closed: {
+      y: "0rem",
+      rotate: "0deg",
+    },
+  };
+};
+
+const burgerTop = makeTopBottom(1, 0.625);
+const burgerMiddle = {
+  open: { opacity: 0 },
+  closed: { opacity: 1 },
+};
+const burgerBottom = makeTopBottom(-1, 0.7);
 
 export default function Header({ children, title }: HeaderProps) {
   const [open, setOpen] = useState(false);
