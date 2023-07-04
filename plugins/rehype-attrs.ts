546efdbdab4e14ac227ebe40eb99a442f3d98a59
Samuel Quiñones <sdquinones1@gmail.com>
2023-07-03 21:48:52 -0400

    added extra logic to handle Astro's special MDX elements

--- plugins/rehype-attrs.ts
+++ plugins/rehype-attrs.ts
@@ -1,13 +1,24 @@
+// TODO: Keep watch for other elements Astro replaces in MDX
+
 import type { RehypePlugin } from "@astrojs/markdown-remark";
 import { visit } from "unist-util-visit";
 
+const jsxFlowWarnMSG = "You are assigning attributes to a JSX flow expression, this is VERY UNSAFE";
+const testFunc = (node: any) => node.type === "element" || node.type === "mdxJsxFlowElement";
+
 export default function rehypeAttrs(): ReturnType<RehypePlugin> {
   return (tree) => {
-    visit(tree, "element", (node, index, parent) => {
-      if (
+    visit(tree, testFunc, (node, index, parent) => {
+      const isElement =
+        node.type === "element" &&
         /^(em|strong|b|a|i|p|pre|kbd|blockquote|h(1|2|3|4|5|6)|code|table|img|del|ul|ol|li)$/.test(
           node.tagName
-        ) &&
+        );
+      const isSpecialJsx =
+        // @ts-expect-error not accounting for MDX
+        node.type === "mdxJsxFlowElement" && /^(__AstroImage__)$/.test(node.name);
+      if (
+        (isElement || isSpecialJsx) &&
         parent &&
         Array.isArray(parent.children) &&
         typeof index === "number" &&
@@ -20,8 +31,16 @@ export default function rehypeAttrs(): ReturnType<RehypePlugin> {
           //@ts-expect-error doesnt account for mdx
           parent.children[index + 1].value.replace(/^\/\*(.*?)\*\/$/, "$1").replace(/^attrs:/, "")
         );
-        for (const [key, value] of searchParams) {
-          node.properties![key] = value;
+        if (isElement) {
+          for (const [key, value] of searchParams) {
+            node.properties![key] = value;
+          }
+        } else {
+          import.meta.env.MODE !== "production" && console.warn(jsxFlowWarnMSG);
+          for (const [key, value] of searchParams) {
+            //@ts-expect-error doesnt account for mdx
+            node.attributes?.push({ name: key, type: "mdxJsxAttribute", value });
+          }
         }
 
         parent.children.splice(index + 1, 1);
--- plugins/rehype-unwrap-images.ts
+++ plugins/rehype-unwrap-images.ts
@@ -1,3 +1,5 @@
+// TODO: try to figure out how to do "some" instead of every
+
 import type { Transformer } from "unified";
 import type { Root } from "hast";
 import { visit, SKIP } from "unist-util-visit";
@@ -9,7 +11,9 @@ export default function rehypeUnwrapImages(): Transformer<Root, Root> {
         node.tagName === "p" &&
         parent &&
         typeof index === "number" &&
-        node.children.every((n) => n.type === "element" && n.tagName === "img")
+        (node.children.every((n) => n.type === "element" && n.tagName === "img") ||
+          // @ts-expect-error not accounting for MDX
+          node.children.every((n) => n.type === "mdxJsxFlowElement" && n.name === "__AstroImage__"))
       ) {
         parent.children.splice(index, 1, ...node.children);
         return [SKIP, index + node.children.length];
