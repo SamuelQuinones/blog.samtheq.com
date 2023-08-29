9a1dc632c99693edec4b5cb0854b877cd69d5c6f
Samuel Quiñones <sdquinones1@gmail.com>
2023-08-28 21:18:01 -0400

    updated rss to not have content b/c mdx

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,7 @@
 import type { APIContext } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
 import { getShowablePosts, sortPosts } from "@util/BlogHelper";
-const parser = new MarkdownIt();
 
 export const prerender = true;
 
@@ -20,7 +17,6 @@ export async function get(context: APIContext) {
       pubDate: post.data.publishDate,
       description: post.data.description,
       link: `/${post.slug}/`,
-      content: sanitizeHtml(parser.render(post.body)),
     })),
     customData: `<language>en-us</language>`,
   });
